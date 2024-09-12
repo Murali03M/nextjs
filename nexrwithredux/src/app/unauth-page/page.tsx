acaa7456a201cde2a67c0caad2fe291ca41f6483
Murali Murugan <murali732000@gmail.com>
2024-09-12 14:17:52 +0100

    add the final correction of build

--- nexrwithredux/src/app/unauth-page/page.tsx
+++ nexrwithredux/src/app/unauth-page/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
 
 
 return <div className="p-20">
-        <h2 className="text-6xl "> You're not loged in , Please login </h2>
+        <h2 className="text-6xl "> Youre not loged in , Please login </h2>
          
         </div>
      
